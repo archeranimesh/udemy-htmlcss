2ba135df03778876bf0bacde5e765d3742cdbef1
Animesh Bhadra <animeshb@archerimagine.com>
2019-08-24 20:02:00 -0600

    Section 07: Edge Ledger, completed JQuery Animation

--- SRC/Section_07_EdgeLedger_Website/edgeledger/js/main.js
+++ SRC/Section_07_EdgeLedger_Website/edgeledger/js/main.js
@@ -11,3 +11,15 @@ var map = new ol.Map({
     })
   });
 //   https://openlayers.org/en/latest/doc/quickstart.html
+
+// Smooth Scrolling animation.
+$('#navbar a, .btn').on('click', function(event){
+  if(this.hash != ''){
+    event.preventDefault();
+    const hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top - 100
+    }, 800);
+  }
+});
