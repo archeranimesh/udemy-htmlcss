3d4c64d0e6204284328ce78fec8c6b61fa947334
Animesh Bhadra <animeshb@archerimagine.com>
2020-04-26 17:43:25 +0530

    adding jquery and smoot scrolling

--- SRC/Section_07_EdgeLedger_Website/edgeledger_V_2.0/js/main.js
+++ SRC/Section_07_EdgeLedger_Website/edgeledger_V_2.0/js/main.js
@@ -16,3 +16,15 @@ window.onscroll = function(){
         scrolled = false;
     }
 }
+
+// Smooth Scrolling animation.
+$('#navbar a, .btn').on('click', function(event){
+    if(this.hash != ''){
+      event.preventDefault();
+      const hash = this.hash;
+  
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 100
+      }, 800);
+    }
+  });
